65835ed7475730f9fccf0199fc4c4a3c479b7799
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2025-05-22 17:16:06 +0200

    added a few features

--- app/src/main/java/com/example/elaborato_mobile/MainActivity.kt
+++ app/src/main/java/com/example/elaborato_mobile/MainActivity.kt
@@ -4,13 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.sharp.AccountCircle
+import androidx.compose.material.icons.sharp.Favorite
+import androidx.compose.material.icons.sharp.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import com.example.elaborato_mobile.ui.theme.Elaborato_MobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +53,55 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Elaborato_MobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    topBar = { TopBar() },
+                    bottomBar = {BottomBar()},
+                    floatingActionButton = {AddButton()}
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding),
+                    ) {
+                        Text("god")
+                        Text("help me")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun BottomBar(){
+    BottomAppBar (
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            var active by remember { mutableStateOf(1) }
+            IconButton( onClick = {active = 1}, modifier = Modifier.padding(20.dp)) {
+                if (active == 1){
+                    Icon(Icons.Filled.Home, "Home")
+                }else{
+                    Icon(Icons.Outlined.Home, "Home")
+                }
+            }
+            IconButton( onClick = {active = 2}, modifier = Modifier.padding(20.dp)) {
+                if (active == 2){
+                    Icon(Icons.Filled.Favorite, "Favorite")
+                }else{
+                    Icon(Icons.Outlined.FavoriteBorder, "Favorite")
+                }
+            }
+            IconButton( onClick = {active = 3}, modifier = Modifier.padding(20.dp)) {
+                if (active == 3){
+                    Icon(Icons.Filled.AccountCircle, "Profile")
+                }else{
+                    Icon(Icons.Outlined.AccountCircle, "Profile")
                 }
             }
         }
@@ -31,17 +109,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun IconToggleButtonSample(filled: ImageVector, outline: ImageVector, desc: String) {
+    var checked by remember { mutableStateOf(false) }
+    IconToggleButton(checked = checked, onCheckedChange = { checked = it }) {
+        if (checked) {
+            Icon(filled, desc)
+        } else {
+            Icon(outline, desc)
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(){
+    TopAppBar(
+        title = { Text("Main page") },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+        actions = {
+            IconButton( onClick = { TODO() }) {
+                Icon(Icons.Filled.Settings, "Settings")
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
+
+
 @Composable
-fun GreetingPreview() {
-    Elaborato_MobileTheme {
-        Greeting("Android")
+fun AddButton(){
+    FloatingActionButton(
+        onClick = { TODO() },
+        containerColor = MaterialTheme.colorScheme.secondary,
+        elevation = FloatingActionButtonDefaults.elevation()
+    ) {
+        Icon(Icons.Filled.Add, "Add")
     }
 }
