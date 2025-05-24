b9ac8fbcaf0d5b9b3e3102136bc3cbd4902be187
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2025-05-24 16:16:48 +0200

    minor changes

--- app/src/main/java/com/example/elaborato_mobile/MainActivity.kt
+++ app/src/main/java/com/example/elaborato_mobile/MainActivity.kt
@@ -9,27 +9,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Lock
-import androidx.compose.material.icons.sharp.AccountCircle
-import androidx.compose.material.icons.sharp.Favorite
-import androidx.compose.material.icons.sharp.Home
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -38,10 +30,8 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -52,8 +42,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.elaborato_mobile.ui.theme.Elaborato_MobileTheme
@@ -72,7 +60,7 @@ class MainActivity : ComponentActivity() {
                     Column(
                         modifier = Modifier.padding(innerPadding),
                     ) {
-                        ScrollableList()
+                        LazyList()
                     }
 
                 }
@@ -133,14 +121,6 @@ fun TopBar(){
     )
 }
 
-@Composable
-fun ScrollableList() {
-    val elems = (0..10).map { "Lista $it" }
-    val scrollState = rememberScrollState()
-    Column(modifier = Modifier.verticalScroll(scrollState).fillMaxWidth()) {
-        for (elem in elems) ListItem(elem)
-    }
-}
 
 @Composable
 fun ListItem(string: String){
@@ -154,6 +134,16 @@ fun ListItem(string: String){
     ) {
         Text(text = string, modifier = Modifier.padding(15.dp), fontSize = 20.sp)
         Text(text = "Prova prova deca è bello è un grande prova test della scritta ", modifier = Modifier.padding(15.dp))
+    }
+}
+
+@Composable
+fun LazyList(){
+    val elems = (0..10).map { "Lista $it" }
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        items(elems.size) { index ->
+            ListItem(elems[index])
+        }
     }
 }
 
